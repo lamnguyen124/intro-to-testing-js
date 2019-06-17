aa27a9084657fd7309bcf1ebd7521c56c1c9f345
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-06-17 16:56:23 -0500

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -1,4 +1,12 @@
 // helloWorld function 
-function helloWorld() {
+const helloWorld = function() {
     return "Hello, World!";
 }
+
+function add (num1, num2) {
+    if (typeof num1 === "string" || typeof num2 === "string") {
+        return false
+    }
+    return num1 + num2;
+}
+function sayHello () {}
